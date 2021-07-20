543a00735842cb80bb010e03ea3ea4c9e61de2ea
Leobardo Arguelles <leoarguelles@pm.me>
2021-07-19 22:44:13 -0700

    Organize files by volume

--- merge.py
+++ merge.py
@@ -4,6 +4,8 @@ import os
 import cli.app
 import cli.log
 import logging
+import re
+from time import sleep
 from os import walk
 from os import path
 from zipfile import ZipFile
@@ -20,6 +22,9 @@ EXTRACT_DIR = '.extracted'
 # Directory with the renamed images, all together, ready to zip
 ZIP_DIR = 'zipper'
 
+# File extension for generated zip files
+ARCHIVE_EXT = '.cbz'
+
 @cli.log.LoggingApp
 def merge(app):
     LOGGER.setLevel(merge.params)
@@ -32,8 +37,8 @@ def merge(app):
         print('Use an absolute path, starting from "/"')
         raise e
 
-    extractCbz(merge.params.path)
-    renameImages()
+    # extractCbz(merge.params.path)
+    # renameImages()
     zipImages()
     print('\nAll done!')
 
@@ -72,46 +77,134 @@ def extractCbz(dir):
 
 def renameImages():
     """
-    Rename images extracted with extractCbz(). The naming convention is
-    <folderName>-<imgNumber>.<extension>
+    Rename images extracted with extractCbz(). The default naming convention is
+    <folderNumber>-<imgNumber>.<extension>, except if --volumize or
+    --chapterize are requested. In those cases:
+    --volumize: <Vol>-<imgNumber>
+    --chapterize: TODO: Determine how this case should be handled.
     """
 
     if not path.isdir(ZIP_DIR):
         os.mkdir(ZIP_DIR)
 
     topDir = os.getcwd()
-    dirCounter = 0
-    for dir in natsorted(os.listdir(EXTRACT_DIR)):
-        counter = 0
-        currentDir = path.join(topDir, EXTRACT_DIR, dir)
-        # Move images to ZIP_DIR, renaming them to be continuous
-        for img in natsorted(os.listdir(currentDir)):
-            name, ext = path.splitext(path.join(currentDir, img))
 
-            LOGGER.info('-'*50)
-            LOGGER.info('Renaming: ' + path.join(currentDir, img) + ' ---> ' + path.join(topDir, ZIP_DIR, str(dirCounter) + '-' + str(counter) + ext))
-            counter += 1
-            copy2(path.join(currentDir, img), path.join(topDir, ZIP_DIR, str(dirCounter) + '-' + str(counter) + ext))
-            LOGGER.info('-'*50)
-        dirCounter += 1
+    if merge.params.volumize:
+        print('volumize detected')
+        for dir in natsorted(os.listdir(EXTRACT_DIR)):
+            print('current dir is: ' + dir)
+            counter = 0
+            currentDir = path.join(topDir, EXTRACT_DIR, dir)
+            print('current dir path is: ' + currentDir)
+            # Move images to ZIP_DIR, renaming them to be continuous
+            for img in natsorted(os.listdir(currentDir)):
+                name, ext = path.splitext(path.join(currentDir, img))
+
+                LOGGER.info('-'*50)
+                LOGGER.info('Renaming: ' + path.join(currentDir, img) + ' ---> ' + path.join(topDir, ZIP_DIR, dir + '-' + str(counter) + ext))
+                counter += 1
+                copy2(path.join(currentDir, img), path.join(topDir, ZIP_DIR, dir + '-' + str(counter) + ext))
+                LOGGER.info('-'*50)
+    # TODO: Refractor to not reuse code. There must be a smarter way to do this.
+    else:
+        dirCounter = 0
+        for dir in natsorted(os.listdir(EXTRACT_DIR)):
+            counter = 0
+            currentDir = path.join(topDir, EXTRACT_DIR, dir)
+            # Move images to ZIP_DIR, renaming them to be continuous
+            for img in natsorted(os.listdir(currentDir)):
+                name, ext = path.splitext(path.join(currentDir, img))
+
+                LOGGER.info('-'*50)
+                LOGGER.info('Renaming: ' + path.join(currentDir, img) + ' ---> ' + path.join(topDir, ZIP_DIR, str(dirCounter) + '-' + str(counter) + ext))
+                counter += 1
+                copy2(path.join(currentDir, img), path.join(topDir, ZIP_DIR, str(dirCounter) + '-' + str(counter) + ext))
+                LOGGER.info('-'*50)
+            dirCounter += 1
 
 
 def zipImages():
     """
-    TODO: Write description
+    Zips the renamed images, using the selected option:
+    DEFAULT: Merge all the images in a single cbz archive.
+    --volumize: Create one archive per volume, following the user provided regex e.g. 'Vol \d+'
+    --chapterize: Create one archive for every <N> chapters, where <N> is indicated by the user.
+    --maxsize: Create the necessary cbz archives, with the restriction that each one must have
+                a size smaller than <M> (provided by the user).
     """
-    ARCHIVE = path.join(os.getcwd(), merge.params.archive)
-    os.chdir(ZIP_DIR)
-    with ZipFile(ARCHIVE + '.cbz', 'w') as zf:
-        LOGGER.info('Compressing images...')
-        for root, dirs, imgs in walk('.'):
-            for img in natsorted(imgs):
-                LOGGER.debug('Adding: ' + img)
-                zf.write(path.join(root, img))
+    topDir = os.getcwd()
+    VOLS_DIR = path.join(topDir, 'zipped_volumes')
+    if not path.isdir(VOLS_DIR):
+        os.mkdir(VOLS_DIR)
+
+    IMGS_DIR = path.join(topDir, ZIP_DIR)
+
+    # Organize chapters by volume
+    if merge.params.volumize:
+        LOGGER.info('Starting to volumize chapters.')
+        # Get stes of pre-sorted volumes keys, and all the images extracted
+        volumes = getVolumes(IMGS_DIR)
+
+        for volume, imgs in volumes.items():
+            LOGGER.info('Archiving chapters of volume: ' + volume)
+            currentArchive = path.join(VOLS_DIR, volume + ARCHIVE_EXT)
+            with ZipFile(currentArchive, 'w') as zf:
+                for img in imgs:
+                    LOGGER.debug('Adding: ' + img)
+                    zf.write(path.join(IMGS_DIR, img))
+    else:
+        ARCHIVE = path.join(os.getcwd(), merge.params.archive)
+        with ZipFile(ARCHIVE + ARCHIVE_EXT, 'w') as zf:
+            LOGGER.info('Compressing images...')
+            for root, dirs, imgs in walk('.'):
+                for img in natsorted(imgs):
+                    LOGGER.debug('Adding: ' + img)
+                    zf.write(path.join(root, img))
+
+
+def getVolumes(dir):
+    """
+    Get the name of all the volumes located in <dir>, using the user-provided
+    regex expression to identify them.
+    Also, get a list of all the chapter's names, (also located in <dir>).
+    :param dir: Path that points to the directory with all the renamed images.
+    :return: A dictionary with volumes as keys, and a set of chapters as the value.
+    """
+    LOGGER.info('Getting dictionary of volumes and chapters')
+    imgs = natsorted(os.listdir(dir))
+    pat = re.compile(merge.params.volumize)
+    currentVol = re.search(pat, imgs[0])[0]
+
+    matches = list()
+    volumes = dict()
+
+    for img in imgs:
+        imgVol = re.search(pat, img)[0]
+        if imgVol == currentVol:
+            matches.append(img)
+            continue
+        volumes[currentVol] = matches
+        # Create a whole new list. Just clearing the same one causes problems,
+        # because all the keys reference the same exact object
+        matches = list()
+        currentVol = imgVol
+        # Don't forget this first image that changed the volume!
+        matches.append(img)
+
+    # Add the last volume, which is not assigned on the loop
+    volumes[currentVol] = matches
+    return volumes
+
+
+
+
+
+
 
 # Script parameters
 merge.add_param('path', help='path to your cbz archives', type=str)
-merge.add_param('archive', help='name of your compressed cbz file', type=str)
+merge.add_param('-a', '--archive', help='name of your compressed cbz file', type=str, default='')
+merge.add_param('-vo', '--volumize', help='generate one archive per volume, using user provided regex', default=False, type=str)
 
 if __name__ == "__main__":
     merge.run()
